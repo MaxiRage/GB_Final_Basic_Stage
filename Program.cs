1b6d7b772f668e2ef070671ddb2c951ef878af48
MaxiRage <iarostiuk@ya.ru>
2022-10-14 00:40:22 +0300

    task_4 done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите массив строк:");
 string[] arrString = Console.ReadLine().Split(" ");
 Console.WriteLine("------------------------------------------");
-Console.WriteLine("Массив из элементов меньше или равных 3:");
+Console.WriteLine("Массив из элементов длиной меньше или равных 3:");
 Array.ForEach(arrIs3OrLess(arrString), Console.WriteLine);
 
 string[] arrIs3OrLess(string[] arrString)
